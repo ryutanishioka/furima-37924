e0816cd3a40c27984a80cfe22cfd27dbc8747c7e
ryutanishioka <105966340+ryutanishioka@users.noreply.github.com>
2022-08-05 02:12:50 +0900

    新規登録・ログイン・ログアウトの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-#  resources :items only: :index
+  resources :users, only: :show
+#  resources :items, only: :index
 end
