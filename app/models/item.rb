6e21dcf3a8ab5b63c3f8c680f73893df07507793
ryutanishioka <105966340+ryutanishioka@users.noreply.github.com>
2022-08-11 01:07:25 +0900

    createアクション~DBへの保存

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,10 +11,14 @@ class Item < ApplicationRecord
 #Active Storage↓
   has_one_attached :image 
 
-  validates :category_id, numericality: { other_than: 1, message: "category can't be blank" }
-  validates :condition_id, numericality: { other_than: 1, message: "condition can't be blank" }
-  validates :payment_id, numericality: { other_than: 1, message: "payment can't be blank" }
-  validates :prefecture_id, numericality: { other_than: 1, message: "prefecture can't be blank" }
-  validates :day_id, numericality: { other_than: 1, message: "day can't be blank" }
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :content, presence: true
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :payment_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :day_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :price, presence: true, format: {with: /\A[0-9]+\z/}, inclusion: { in: 300..9999999 }
 
 end
