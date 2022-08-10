8fb4d9e9387534d68bbe434ee094f92b40ce0496
ryutanishioka <105966340+ryutanishioka@users.noreply.github.com>
2022-08-10 21:34:54 +0900

    Active Storageの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,12 +2,14 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
   belongs_to :user
+#Active Hash↓
   belongs_to :category
   belongs_to :condition
   belongs_to :payment
   belongs_to :prefecture
   belongs_to :day
-  #バリデーションの設定忘れずに
+#Active Storage↓
+  has_one_attached :image 
 
   validates :category_id, numericality: { other_than: 1, message: "category can't be blank" }
   validates :condition_id, numericality: { other_than: 1, message: "condition can't be blank" }
