79ba353abb2abd596be12fd167636ef61a10e468
ryutanishioka <105966340+ryutanishioka@users.noreply.github.com>
2022-08-15 16:59:21 +0900

    必要に応じてログイン画面やトップページに遷移する実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :redirect_to_index, only: [:edit]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -41,5 +42,12 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :content, :price, :image, :category_id,
       :condition_id, :payment_id, :prefecture_id, :day_id).merge(user_id: current_user.id)
+  end
+
+  def redirect_to_index
+    @item = Item.find(params[:id])
+    unless current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 end
