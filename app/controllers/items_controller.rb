d79dc316762c87491349fa68b46c25dff418c3ce
ryutanishioka <105966340+ryutanishioka@users.noreply.github.com>
2022-08-17 22:29:53 +0900

    削除機能の実装と削除先のパスの指定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
-  before_action :item_find, only: [:show, :edit, :update]
-  before_action :redirect_to_index, only: [:edit]
+  before_action :item_find, only: [:show, :edit, :update, :destroy]
+  before_action :redirect_to_index, only: [:edit, :destroy]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -35,6 +35,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    @item.destroy
+    redirect_to root_path
   end
 
   private
