f942b797d67d81671d0088096573ec32a270b4fd
ryutanishioka <105966340+ryutanishioka@users.noreply.github.com>
2022-08-23 11:29:00 +0900

    バリデーションの記述

--- app/models/send.rb
+++ app/models/send.rb
@@ -1,3 +1,9 @@
 class Send < ApplicationRecord
   belongs_to :order
+
+  validates :post_code, presence: true, format: {/\A\d{3}[-]\d{4}\z/}
+  validates :city, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true, format: {/\A\d{10,11}\z/}
+  validates :prefecture_id, presence: true
 end
